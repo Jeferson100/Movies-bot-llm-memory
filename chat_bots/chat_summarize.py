b39b8d8ad2e6d77aedd5cb02dc45f090eaab2f6c
JEFERSON DIONEI SEHNEM <sehnemjeferson@gmail.com>
2024-12-23 16:40:36 -0300

    Adcionando chaves apis

--- chat_bots/chat_summarize.py
+++ chat_bots/chat_summarize.py
@@ -10,8 +10,9 @@ from .verificao_key import get_secret_key
 
 load_dotenv()
 
-api_secret_groq = get_secret_key("GROQ_API_KEY")
-if api_secret_groq is None:
+try:
+    api_secret_groq = get_secret_key("GROQ_API_KEY")
+except KeyError:
     raise ValueError("API key inválida ou não definida")
 
 
